2c464aa8f0c045ff1b6702d993c93a4d50c17b26
adbenitez <asieldbenitez@gmail.com>
2023-01-31 03:02:54 -0500

    remove rpc property from Contact and Message objects

--- deltachat/contact.go
+++ deltachat/contact.go
@@ -3,11 +3,10 @@ package deltachat
 // Delta Chat Contact.
 type Contact struct {
 	acc *Account
-	rpc *Rpc
 	Id  uint64
 }
 
 // Contact factory
 func NewContact(acc *Account, id uint64) Contact {
-	return Contact{acc, acc.rpc, id}
+	return Contact{acc, id}
 }
--- deltachat/message.go
+++ deltachat/message.go
@@ -3,14 +3,13 @@ package deltachat
 // Delta Chat Message.
 type Message struct {
 	acc *Account
-	rpc *Rpc
 	Id  uint64
 }
 
 // Return map of this account configuration parameters.
 func (msg Message) GetSnapshot() (map[string]any, error) {
 	var snapshot map[string]any
-	err := msg.rpc.CallResult(&snapshot, "get_message", msg.acc.Id, msg.Id)
+	err := msg.acc.rpc.CallResult(&snapshot, "get_message", msg.acc.Id, msg.Id)
 	snapshot["chat"] = NewChat(msg.acc, uint64(snapshot["chatId"].(float64)))
 	snapshot["sender"] = NewContact(msg.acc, uint64(snapshot["fromId"].(float64)))
 	snapshot["message"] = msg
@@ -19,10 +18,10 @@ func (msg Message) GetSnapshot() (map[string]any, error) {
 
 // Mark the message as seen.
 func (msg Message) MarkSeen() error {
-	return msg.rpc.Call("markseen_msgs", msg.acc.Id, []any{msg.Id})
+	return msg.acc.rpc.Call("markseen_msgs", msg.acc.Id, []any{msg.Id})
 }
 
 // Message factory
 func newMessage(acc *Account, id uint64) Message {
-	return Message{acc, acc.rpc, id}
+	return Message{acc, id}
 }
