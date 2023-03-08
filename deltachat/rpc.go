8db8ab8f98f9ec53772636e7262bbfc86f3898a2
adbenitez <asieldbenitez@gmail.com>
2023-03-08 03:31:21 -0500

    avoid panics writing and reading at the same time from RpcIO.events map

--- deltachat/rpc.go
+++ deltachat/rpc.go
@@ -82,7 +82,7 @@ func (self *RpcIO) Start() error {
 
 	self.ctx = context.Background()
 	self.events = make(map[uint64]chan *Event)
-	options := jrpc2.ClientOptions{OnNotify: self._onNotify}
+	options := jrpc2.ClientOptions{OnNotify: self.onNotify}
 	self.client = jrpc2.NewClient(channel.Line(stdout, self.stdin), &options)
 	return nil
 }
@@ -110,8 +110,7 @@ func (self *RpcIO) Stop() {
 }
 
 func (self *RpcIO) GetEventChannel(accountId uint64) <-chan *Event {
-	self._initEventChannel(accountId)
-	return self.events[accountId]
+	return self.getEventChannel(accountId)
 }
 
 func (self *RpcIO) Call(method string, params ...any) error {
@@ -123,21 +122,24 @@ func (self *RpcIO) CallResult(result any, method string, params ...any) error {
 	return self.client.CallResult(self.ctx, method, params, &result)
 }
 
-func (self *RpcIO) _initEventChannel(accountId uint64) {
+func (self *RpcIO) getEventChannel(accountId uint64) chan *Event {
+	defer self.eventsMutex.Unlock()
 	self.eventsMutex.Lock()
-	if _, ok := self.events[accountId]; !ok {
-		self.events[accountId] = make(chan *Event, 10)
+	channel, ok := self.events[accountId]
+	if !ok {
+		channel = make(chan *Event, 10)
+		self.events[accountId] = channel
 	}
-	self.eventsMutex.Unlock()
+	return channel
 }
 
-func (self *RpcIO) _onNotify(req *jrpc2.Request) {
+func (self *RpcIO) onNotify(req *jrpc2.Request) {
 	if req.Method() == "event" {
 		var params _Params
 		req.UnmarshalParams(&params)
-		self._initEventChannel(params.ContextId)
+		channel := self.getEventChannel(params.ContextId)
 		if !self.closed {
-			go func() { self.events[params.ContextId] <- params.Event }()
+			go func() { channel <- params.Event }()
 		}
 	}
 }
