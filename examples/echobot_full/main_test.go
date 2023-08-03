00435935e0d28ac9bbc31a0d21794d9bdf40f744
adbenitez <asieldbenitez@gmail.com>
2023-08-03 02:47:09 +0200

    update example

--- examples/echobot_full/main_test.go
+++ examples/echobot_full/main_test.go
@@ -1,7 +1,6 @@
 package main // replace with your package name
 
 import (
-	"log"
 	"testing"
 
 	"github.com/deltachat/deltachat-rpc-client-go/deltachat"
@@ -13,9 +12,5 @@ func TestMain(m *testing.M) {
 	acfactory = &deltachat.AcFactory{}
 	acfactory.TearUp()
 	defer acfactory.TearDown()
-	acfactory.WithRpc(func(rpc *deltachat.Rpc) {
-		sysinfo, _ := rpc.GetSystemInfo()
-		log.Println("Running deltachat core", sysinfo["deltachat_core_version"])
-	})
 	m.Run()
 }
