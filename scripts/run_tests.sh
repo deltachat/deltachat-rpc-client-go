4ccc148ec340c3ec8281a47d756177676d8c1709
adbenitez <asieldbenitez@gmail.com>
2024-02-04 18:55:50 -0500

    update run_tests.sh, use more recent courtney

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,5 +1,6 @@
 #!/bin/env bash
 
+echo "Checking code with gofmt..."
 OUTPUT=`gofmt -d .`
 if [ -n "$OUTPUT" ]
 then
@@ -7,8 +8,10 @@ then
     exit 1
 fi
 
+echo "Checking code with golangci-lint..."
 if ! command -v golangci-lint &> /dev/null
 then
+    echo "golangci-lint not found, installing..."
     # binary will be $(go env GOPATH)/bin/golangci-lint
     curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.55.2
 fi
@@ -20,7 +23,7 @@ fi
 
 if ! command -v deltachat-rpc-server &> /dev/null
 then
-    echo "deltachat-rpc-server could not be found, installing..."
+    echo "deltachat-rpc-server not found, installing..."
     curl -L https://github.com/deltachat/deltachat-core-rust/releases/latest/download/deltachat-rpc-server-x86_64-linux --output deltachat-rpc-server
     chmod +x deltachat-rpc-server
     export PATH=`pwd`:"$PATH"
@@ -28,8 +31,8 @@ fi
 
 if ! command -v courtney &> /dev/null
 then
-    echo "courtney could not be found, installing..."
-    go install github.com/dave/courtney@latest
+    echo "courtney not found, installing..."
+    go install github.com/dave/courtney@master
 fi
 
 for i in ./examples/*.go
